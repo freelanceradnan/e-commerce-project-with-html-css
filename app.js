f574df090dc8cbbb579cde505c8191fe0f0e0730
freelanceradnan <reactorbro722@gmail.com>
2025-12-24 20:35:14 +0600

    product added with fetch api

--- app.js
+++ app.js
@@ -12,3 +12,29 @@ if(close){
     nav.classList.remove('active')
     })
 }
+//fetch api
+const productsDiv = document.querySelector('.products');
+
+fetch('https://fakestoreapi.com/products')
+  .then(res => res.json())
+  .then(products => {
+    productsDiv.innerHTML = ""; 
+
+    products.forEach(product => {
+      productsDiv.innerHTML += `
+        <div class="product-card">
+          <img src="${product.image}" alt="${product.title}">
+          <span>${product.category}</span>
+          <h5>${product.title}</h5>
+          <div class="star">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+          </div>
+          <h4>$${product.price}</h4>
+        </div>
+      `;
+    });
+  });
