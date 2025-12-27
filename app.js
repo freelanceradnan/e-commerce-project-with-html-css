7d0c2bfcf18d46153b8065fe03d8e61052b57ff7
freelanceradnan <reactorbro722@gmail.com>
2025-12-27 23:15:45 +0600

    loader animation in product section added

--- app.js
+++ app.js
@@ -13,19 +13,34 @@ if(close){
     })
 }
 //fetch api
-const productsDiv = document.querySelector('.products');
+// const productsDiv = document.querySelector('.products');
 
-fetch('https://fakestoreapi.com/products')
-  .then(res => res.json())
-  .then(products => {
-    productsDiv.innerHTML = ""; 
+// fetch('')
+//   .then(res => res.json())
+//   .then(products => {
+//     productsDiv.innerHTML = ""; 
 
-    products.forEach(product => {
-      productsDiv.innerHTML += `
+//    (product => {
+//      
+//     });
+//   });
+document.addEventListener('DOMContentLoaded',()=>{
+let productsDiv=document.querySelector('.products')
+let loader=document.getElementById('loading')
+async function fetchApiData(url){
+ 
+  try {
+     loader.style.display="flex"
+  const res=await fetch(url)
+  const data=await res.json()
+  let product=data
+ product.forEach(product => {
+   productsDiv.innerHTML += `
         <div class="product-card">
-          <img src="${product.image}" alt="${product.title}">
+        <img src="${product.image}" alt="${product.title}">
           <span>${product.category}</span>
           <h5>${product.title}</h5>
+          <div class="star">
           <div class="star">
             <i class="fa fa-star"></i>
             <i class="fa fa-star"></i>
@@ -36,5 +51,13 @@ fetch('https://fakestoreapi.com/products')
           <h4>$${product.price}</h4>
         </div>
       `;
-    });
-  });
+ });
+  } catch (error) {
+    console.log(error)
+  }
+  finally{
+    loader.style.display="none"
+  }
+}
+fetchApiData('https://fakestoreapi.com/products')
+})
